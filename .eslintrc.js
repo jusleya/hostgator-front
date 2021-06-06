1518d0e191fdf55d487dde261c94f433acce12da
unknown <jusleytavares@ioasys.com.br>
2021-06-06 20:24:07 -0300

    correction prettier/prettier eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:prettier/recommended', 'prettier/react', 'airbnb'],
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended', 'prettier', 'airbnb'],
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 12,
